d127cd47857e5f897c5c8261cb89bdd53ea9f136
Charles Averill <charlesaverill20@gmail.com>
2020-05-27 14:20:36 -0500

    Save execute output to text file

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import io as StringIO
 import tkinter as tk
+
+import sys
+import contextlib
 
 print("------------------------------------------------------------------------\n"
       "Hi, and welcome to Satyrn.\n"
@@ -20,16 +24,15 @@ provide functionality specific to our needs.
 The Interpreter is the 'frontend' of the application, and provides a text-based interface for users to interact with
 the graph and individual nodes.
 
-TextInput is just a handy way of taking multi-line input from users. 
+TextIO is just a handy way of taking multi-line input from users and displaying text back to them.
 """
 
 
-class TextInput:
+class TextIO:
 
     def __init__(self):
         # Root tkinter window
-        self.root = tk.Tk()
-        self.root.wm_title("Satyrn Text Editor")
+        self.root = None
         self.user_input = ""
 
     def get_text_from_widget(self, widget):
@@ -40,6 +43,8 @@ class TextInput:
         self.root.quit()
 
     def text_input(self, existing_text=None):
+        self.root = tk.Tk()
+        self.root.wm_title("Satyrn Text Editor")
         # Open text input window and return the text
         text = tk.Text(self.root)
         if existing_text:
@@ -75,13 +80,34 @@ class Cell:
         self.content = content_
         self.graph = graph_
 
-    def execute(self):
+    @contextlib.contextmanager
+    def stdoutIO(self, stdout=None):
+        old = sys.stdout
+        if stdout is None:
+            stdout = StringIO.StringIO()
+        sys.stdout = stdout
+        yield stdout
+        sys.stdout = old
+
+    def execute(self, stdout):
         # Execute this cell's content
-        try:
-            exec(self.content, self.graph.exec_globals)
-        except Exception as e:
-            print("Exception occurred in cell " + self.name)
-            print(e)
+        if stdout == "internal":
+            try:
+                exec(self.content, self.graph.exec_globals)
+            except Exception as e:
+                print("Exception occurred in cell " + self.name)
+                print(e)
+        elif stdout == "external":
+            try:
+                with self.stdoutIO() as s:
+                    exec(self.content, self.graph.exec_globals)
+                    return s.getvalue()
+            except Exception as e:
+                print("Exception occurred in cell " + self.name)
+                print(e)
+        else:
+            print("stdout setting \"" + stdout + "\" not recognized. Please use internal/external.")
+            return
 
 
 class Graph:
@@ -93,6 +119,8 @@ class Graph:
         self.names_to_indeces = {}
         # Dictionary for variables created by cells
         self.exec_globals = {}
+        # TextIO object
+        self.ti = TextIO()
 
     def reset_runtime(self):
         # Erase all runtime variables
@@ -209,14 +237,17 @@ class Graph:
 
         plt.show()
 
-    def execute_linear_list_of_cells(self, cells_list=None):
+    def execute_linear_list_of_cells(self, cells_list=None, stdout="internal", output_filename="stdout.txt"):
         if not cells_list:
             cells_list = list(nx.get_node_attributes(self.graph, 'name').values())
 
         for cell_name in cells_list:
             cell = self.get_cell(cell_name)
             if cell.content_type == "python":
-                cell.execute()
+                cell_output = cell.execute(stdout)
+                if stdout == "external":
+                    with open(output_filename, 'w') as txt:
+                        txt.write(cell_output)
 
 
 # noinspection PyBroadException
@@ -229,6 +260,9 @@ class Interpreter:
         self.input_type = "live"
         # This will be set if the user executes a .satx file
         self.file = None
+        # This determines whether or not stdout gets sent to an external textbox
+        self.stdout = "internal"
+        self.stdout_filename = "stdout.txt"
         # Start loop
         self.run()
 
@@ -289,7 +323,7 @@ class Interpreter:
                     content += temp
                     temp = self.file.pop(0) + "\n"
             else:
-                ti = TextInput()
+                ti = TextIO()
                 content = ti.text_input().strip()
 
         self.graph.add_cell(Cell(name, self.graph, content_type, content))
@@ -305,7 +339,7 @@ class Interpreter:
         target_cell = self.graph.get_cell(command[1])
         old_content = target_cell.content
 
-        ti = TextInput()
+        ti = TextIO()
         new_content = ti.text_input(old_content).strip()
 
         target_cell.content = new_content
@@ -371,13 +405,17 @@ class Interpreter:
         """
         if len(command) > 1:
             try:
-                cells_list = command[1:]
-                self.graph.execute_linear_list_of_cells(cells_list)
+                if ">>" in command:
+                    cells_list = command[1:-2]
+                    self.stdout_filename = command[-1]
+                else:
+                    cells_list = command[1:]
+                self.graph.execute_linear_list_of_cells(cells_list, self.stdout, self.stdout_filename)
             except Exception as e:
                 print("There was an error executing one of the cells")
                 print(e)
         else:
-            self.graph.execute_linear_graph()
+            self.graph.execute_linear_list_of_cells(None, self.stdout)
 
     def display(self, command):
         """
@@ -391,6 +429,15 @@ class Interpreter:
                 return
             else:
                 print(self.graph.get_cell(command[1]).content)
+
+    def set_stdout(self, command):
+        """
+        :param command: command to be executed
+        """
+        if len(command) != 2 or (not command[1] == "internal" and not command[1] == "external"):
+            print("stdout takes 1 arguments: (internal/external)")
+            return
+        self.stdout = command[1]
 
     def reset_runtime(self):
         # Delete all runtime variables
@@ -416,6 +463,9 @@ class Interpreter:
             elif command[0] == "edit":
                 self.edit_cell(command)
 
+            elif command[0] == "remove_cell":
+                self.remove_cell(command)
+
             elif command[0] == "link":
                 self.link(command)
 
@@ -431,8 +481,8 @@ class Interpreter:
             elif command[0] == "display":
                 self.display(command)
 
-            elif command[0] == "remove_cell":
-                self.remove_cell(command)
+            elif command[0] == "stdout":
+                self.set_stdout(command)
 
             elif command[0] == "reset_runtime" and len(command) == 1:
                 self.reset_runtime()
