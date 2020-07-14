0f016da6ce8ba9d0827db08500a110b89326cd29
tsaranon <charlesaverill20@gmail.com>
2020-07-13 21:40:54 -0500

    Branching supported

---
+++ build/lib/satyrn_python/__main__.py
@@ -0,0 +1,4 @@
+from .interpreter import start
+
+if __name__ == "__main__":
+    start()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='satyrn-python',
-    version='0.5.5',
+    version='0.6.1',
     entry_points={"console_scripts": ["satyrn = satyrn_python.__main__:start"]},
     author="Charles Averill",
     author_email="charlesaverill20@gmail.com",
