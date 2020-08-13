ab561cac4ab4442a072b2ea86a8b94172a60750d
CharlesAverill <charlesaverill20@gmail.com>
2020-08-12 22:40:48 -0500

    __init__ style update

--- satyrn_python/__init__.py
+++ satyrn_python/__init__.py
@@ -1,5 +1,4 @@
-"""
-Edit python code in a web IDE that supports backend multithreading and network collaboration.
+"""Edit python code in a web IDE that supports backend multithreading and network collaboration.
 
 Visit https://github.com/CharlesAverill/satyrn or https://satyrn.readthedocs.io/ for documentation.
 """
