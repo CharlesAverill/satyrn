872faad7c186d8aa4e317972852ab6b449a3d7ff
CharlesAverill <charlesaverill20@gmail.com>
2020-08-12 22:35:01 -0500

    __init__ style update

--- satyrn_python/__init__.py
+++ satyrn_python/__init__.py
@@ -1,3 +1,8 @@
+"""
+Edit python code in a web IDE that supports backend multithreading and network collaboration.
+
+Visit https://github.com/CharlesAverill/satyrn or https://satyrn.readthedocs.io/ for documentation.
+"""
 from .entry_point import main
 
 __all__ = ["main"]
